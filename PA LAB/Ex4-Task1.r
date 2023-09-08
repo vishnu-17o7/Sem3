00f13103a2212f118f0da90b2e1dc0f97d777b85
Vishnu <88884840+vishnu-17o7@users.noreply.github.com>
2023-09-08 10:26:01 +0530

    Add files via upload

---
+++ PA LAB/Ex4-Task1.r
@@ -0,0 +1,9 @@
+x1 <- c(91, 97, 108, 121, 67, 124, 51, 73, 111, 57)
+x2 <- c(71, 75, 69, 97, 70, 91, 39, 61, 80, 47)
+
+print(cov(x1,x2))
+print(cor(x1,x2))
+
+mat <- matrix(c(x1, x2), ncol = 2)
+print(cov(mat))
+print(cor(mat))
