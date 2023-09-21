5436c22e974ee3c610284b39d7a4ad826f4222d2
Vishnu <vishnu.aadhya@gmail.com>
2023-09-21 11:14:22 +0530

    reall

--- AI LAb/vacuumCleaner.py
+++ AI LAb/vacuumCleaner.py
@@ -1,18 +1,23 @@
-def clean(current_state,goal_state):
+def clean(current_state, goal_state, cost):
     for item in current_state:
-        print("Cleaning place :",item)
+        print("Cleaning place:", item)
         if current_state[item] > 0:
-            current_state[item]=current_state[item] - 1
-            
-
+            current_state[item] = current_state[item] - 1
+            cost = cost + 1
+    if current_state != goal_state:
+        cost = clean(current_state, goal_state, cost)
+    return cost
 
 current_state = {}
 goal_state = {}
-n = int(input("Enter the number of Locations :"))
+n = int(input("Enter the number of Locations: "))
+
 for i in range(n):
-    place = input("Enter location name :")
-    state = int(input("Enter the state of the place (0-Clean, 1-Dirty):"))
-    current_state[place]=state
-    goal_state[place]=0
-#print(current_state)
-clean(current_state, goal_state)
+    place = input("Enter location name: ")
+    state = int(input("Enter the state of the place (0-Clean, 1-Dirty): "))
+    current_state[place] = state
+    goal_state[place] = 0
+
+cost = 0
+final_cost = clean(current_state, goal_state, cost)
+print("Total cost of cleaning:", final_cost)
