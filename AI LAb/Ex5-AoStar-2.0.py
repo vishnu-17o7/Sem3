51b7b781d73d67827dcfdf71229f67b8e755a7ea
Vishnu <vishnu.aadhya@gmail.com>
2023-10-03 18:07:29 +0530

    real

---
+++ AI LAb/Ex5-AoStar-2.0.py
@@ -0,0 +1,75 @@
+# Cost to find the AND and OR path
+def Cost(H, condition, weight = 1):
+	cost = {}
+	if 'AND' in condition:
+		AND_nodes = condition['AND']
+		Path_A = ' AND '.join(AND_nodes)
+		PathA = sum(H[node]+weight for node in AND_nodes)
+		cost[Path_A] = PathA
+
+	if 'OR' in condition:
+		OR_nodes = condition['OR']
+		Path_B =' OR '.join(OR_nodes)
+		PathB = min(H[node]+weight for node in OR_nodes)
+		cost[Path_B] = PathB
+	return cost
+
+# Update the cost
+def update_cost(H, Conditions, weight=1):
+	Main_nodes = list(Conditions.keys())
+	Main_nodes.reverse()
+	least_cost= {}
+	for key in Main_nodes:
+		condition = Conditions[key]
+		print(key,':', Conditions[key],'>>>', Cost(H, condition, weight))
+		c = Cost(H, condition, weight)
+		H[key] = min(c.values())
+		least_cost[key] = Cost(H, condition, weight)		
+	return least_cost
+
+# Print the shortest path
+def shortest_path(Start,Updated_cost, H):
+	Path = Start
+	if Start in Updated_cost.keys():
+		Min_cost = min(Updated_cost[Start].values())
+		key = list(Updated_cost[Start].keys())
+		values = list(Updated_cost[Start].values())
+		Index = values.index(Min_cost)
+		
+		# FIND MINIMIMUM PATH KEY
+		Next = key[Index].split()
+		# ADD TO PATH FOR OR PATH
+		if len(Next) == 1:
+
+			Start =Next[0]
+			Path += '<--' +shortest_path(Start, Updated_cost, H)
+		# ADD TO PATH FOR AND PATH
+		else:
+			Path +='<--('+key[Index]+') '
+
+			Start = Next[0]
+			Path += '[' +shortest_path(Start, Updated_cost, H) + ' + '
+
+			Start = Next[-1]
+			Path += shortest_path(Start, Updated_cost, H) + ']'
+
+	return Path
+		
+		
+
+H = {'S': 0,'A': 7, 'B': 12, 'C': 13, 'D': 5, 'E': 6, 'F': 5, 'G': 7, 'H': 2}
+
+Conditions = {
+'S': {'OR': ['C'], 'AND': ['A', 'B']},
+'A': {'AND': ['D', 'E']},
+'C': {'AND': ['F','G']},
+'D': {'OR' : ['H']}
+
+}
+# weight
+weight = 1
+# Updated cost
+print('Updated Cost :')
+Updated_cost = update_cost(H, Conditions, weight=1)
+print('*'*75)
+print('Shortest Path :\n',shortest_path('S', Updated_cost,H))
