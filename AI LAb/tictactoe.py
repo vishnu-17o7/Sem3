881a507622b63382e9059efea5fcbb65857ef133
Vishnu <vishnu.aadhya@gmail.com>
2023-11-03 07:19:04 +0530

    pls

--- AI LAb/tictactoe.py
+++ AI LAb/tictactoe.py
@@ -1,87 +1,113 @@
-player, opponent = 'x', 'o'
-
-def minimax(board, depth, isMax) :
-    score = evaluate(board)
-
-    if score == 10:
-        return score
-
-    if score == -10:
-        return score
-
-    if not isMovesLeft(board):
-        return 0
-
-    if isMax:
-        best = -1000
-        for i in range(3):
-            for j in range(3):
-                if board[i][j] == '_':
-                    board[i][j] = player
-                    best = max(best, minimax(board, depth + 1, not isMax))
-                    board[i][j] = '_'
-        return best
-    else:
-        best = 1000
-        for i in range(3):
-            for j in range(3):
-                if board[i][j] == '_':
-                    board[i][j] = opponent
-                    best = min(best, minimax(board, depth + 1, not isMax))
-                    board[i][j] = '_'
-        return best
-
-def findBestMove(board):
-    bestVal = -1000
-    bestMove = (-1, -1)
+def isMovesLeft(b):
     for i in range(3):
         for j in range(3):
-            if board[i][j] == '_':
-                board[i][j] = player
-                moveVal = minimax(board, 0, False)
-                board[i][j] = '_'
-                if moveVal > bestVal:
-                    bestMove = (i, j)
-                    bestVal = moveVal
-    print("The value of the best Move is:", bestVal)
-    return bestMove
-
-def isMovesLeft(board):
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == '_':
+            if b[i][j] == '_':
                 return True
     return False
 
-def evaluate(b):
-    for col in range(3):
-        if b[0][col] == b[1][col] and b[1][col] == b[2][col]:
-            if b[0][col] == player:
-                return 10
-            elif b[0][col] == opponent:
-                return -10
+def evaluate(b, player, opponent):
     for row in range(3):
-        if b[row][0] == b[row][1] and b[row][1] == b[row][2]:
+        if b[row][0] == b[row][1] == b[row][2]:
             if b[row][0] == player:
                 return 10
             elif b[row][0] == opponent:
                 return -10
-    if b[0][0] == b[1][1] and b[1][1] == b[2][2]:
+    for col in range(3):
+        if b[0][col] == b[1][col] == b[2][col]:
+            if b[0][col] == player:
+                return 10
+            elif b[0][col] == opponent:
+                return -10
+    if b[0][0] == b[1][1] == b[2][2]:
         if b[0][0] == player:
             return 10
         elif b[0][0] == opponent:
             return -10
-    if b[0][2] == b[1][1] and b[1][1] == b[2][0]:
+    if b[0][2] == b[1][1] == b[2][0]:
         if b[0][2] == player:
             return 10
         elif b[0][2] == opponent:
             return -10
     return 0
 
+def minimax(b, depth, isMax, player, opponent):
+    score = evaluate(b, player, opponent)
+    if score == 10:
+        return score
+    if score == -10:
+        return score
+    if not isMovesLeft(b):
+        return 0
+
+    if isMax:
+        best = -1000
+        for i in range(3):
+            for j in range(3):
+                if b[i][j] == '_':
+                    b[i][j] = player
+                    best = max(best, minimax(b, depth + 1, not isMax, player, opponent))
+                    #print(best)
+                    b[i][j] = '_'
+        return best
+    else:
+        best = 1000
+        for i in range(3):
+            for j in range(3):
+                if b[i][j] == '_':
+                    b[i][j] = opponent
+                    best = min(best, minimax(b, depth + 1, not isMax, player, opponent))
+                    #print(best)
+                    b[i][j] = '_'
+        return best
+
+def findBestMove(b, player, opponent):
+    bestVal = -1000
+    bestMove = (-1, -1)
+    for i in range(3):
+        for j in range(3):
+            if b[i][j] == '_':
+                b[i][j] = player
+                moveVal = minimax(b, 0, False, player, opponent)
+                b[i][j] = '_'
+                if moveVal > bestVal:
+                    bestMove = (i, j)
+                    bestVal = moveVal
+    return bestMove
+
+def printBoard(b):
+    for row in b:
+        print(" ".join(row))
+
+player = 'x'
+opponent = 'o'
 board = [
-    ['x', 'o', 'x'],
-    ['o', '_', 'x'],
-    ['_', '_', 'o']
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_']
 ]
-bestMove = findBestMove(board)
-print("The Optimal Move is:\n", "ROW:", bestMove[0], "COL:", bestMove[1])
+
+while True:
+    printBoard(board)
+    row = int(input("Enter the row (0, 1, or 2): "))
+    col = int(input("Enter the column (0, 1, or 2): "))
+    
+    if board[row][col] == '_':
+        board[row][col] = player
+    else:
+        print("Invalid move. Try again.")
+        continue
+
+    if evaluate(board, player, opponent) == 10:
+        print("You win!")
+        break
+
+    if not isMovesLeft(board):
+        print("It's a draw!")
+        break
+
+    bestMove = findBestMove(board, player, opponent)
+    board[bestMove[0]][bestMove[1]] = opponent
+
+    if evaluate(board, player, opponent) == -10:
+        print("AI wins!")
+        break
--- JAVA ONE C/WorldDB.java
+++ JAVA ONE C/WorldDB.java
@@ -1,5 +1,4 @@
 import java.sql.*;
-
 public class WorldDB {
      public static void main(String args[]){
         try{
@@ -10,7 +9,7 @@ public class WorldDB {
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery("select country.name, country.continent, country.region, countryLanguage.Language from country inner join countrylanguage on country.code = countrylanguage.countrycode;");
             while(rs.next())
-            System.out.println(rs.getString(1)+"   "+rs.getString(2)+"   "+rs.getString(3)+"   "+rs.getString(4));
+            System.out.println(rs.getString(0)+"   "+rs.getString(2)+"   "+rs.getString(3)+"   "+rs.getString(4));
             con.close();
         }catch(Exception e){
             System.out.println(e);}
