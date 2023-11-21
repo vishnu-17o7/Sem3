cac166dfc68b11e330df4bd0391f94cf959faaa8
Vishnu <vishnu.aadhya@gmail.com>
2023-11-21 15:27:20 +0530

    hi

--- JAVA ONE C/MysqlCon.java
+++ JAVA ONE C/MysqlCon.java
@@ -10,7 +10,7 @@ class MysqlCon{
             while(rs.next())
             System.out.println(rs.getInt(1)+"   "+rs.getString(2)+"   "+rs.getString(3)+"   "+rs.getString(4));
             con.close();
-        }catch(BankTransactionExample e){
+        }catch(Exception e){
             System.out.println(e);}
         }
 }
--- JAVA ONE C/WorldDB.java
+++ JAVA ONE C/WorldDB.java
@@ -10,7 +10,7 @@ public class WorldDB {
             while(rs.next())
             System.out.println(rs.getString(1)+"   "+rs.getString(2)+"   "+rs.getString(3)+"   "+rs.getString(4));
             con.close();
-        }catch(BankTransactionExample e){
+        }catch(Exception e){
             System.out.println(e);}
         }
 }
